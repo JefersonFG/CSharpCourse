382439d885d7fcaad84c6f0118f666b237c529dc
Jéferson Ferreira Guimarães <jefersonfguimaraes@gmail.com>
2015-04-10 10:41:50 -0300

    First assignment done, created variables for student and course and printed them on console.

--- LabProject/Program.cs
+++ LabProject/Program.cs
@@ -10,6 +10,82 @@ namespace LabProject
     {
         static void Main(string[] args)
         {
+            String studentFirstName;
+            String studentLastName;
+            String studentBirthday;
+            String studentAddressLine1;
+            String studentAddressLine2;
+            String studentCity;
+            String studentStateProvince;
+            String studentCountry;
+            int studentZipPostal;
+
+            String professorFirstName;
+            String professorLastName;
+            String professorTeachingSubject;
+
+            String degreeName;
+            int degreeRequiredCredits;
+            int degreeWorkload;
+
+            String programName;
+            String programDegrees;
+            String programDepartmentHead;
+
+            String courseName;
+            String courseCondition;
+
+            studentFirstName = "Jéferson";
+            studentLastName = "Guimarães";
+            studentBirthday = "02/08/1995";
+            studentAddressLine1 = "Rua Joaquim Gonçalves Ledo";
+            studentAddressLine2 = "Canudos";
+            studentCity = "Novo Hamburgo";
+            studentStateProvince = "RS";
+            studentCountry = "Brazil";
+            studentZipPostal = 93542500;
+
+            professorFirstName = "Gerry";
+            professorLastName = "O'Brien";
+            professorTeachingSubject = "Programming with C#";
+
+            degreeName = "Bachelor in Computer Engineering";
+            degreeRequiredCredits = 156;
+            degreeWorkload = 2640;
+
+            programName = "Computation";
+            programDegrees = "Master, Ph.D.";
+            programDepartmentHead = "IT Institute";
+
+            courseName = "Software Engineering";
+            courseCondition = "Required";
+
+            Console.WriteLine(studentFirstName);
+            Console.WriteLine(studentLastName);
+            Console.WriteLine(studentBirthday);
+            Console.WriteLine(studentAddressLine1);
+            Console.WriteLine(studentAddressLine2);
+            Console.WriteLine(studentCity);
+            Console.WriteLine(studentStateProvince);
+            Console.WriteLine(studentCountry);
+            Console.WriteLine(studentZipPostal);
+
+            Console.WriteLine(professorFirstName);
+            Console.WriteLine(professorLastName);
+            Console.WriteLine(professorTeachingSubject);
+
+            Console.WriteLine(degreeName);
+            Console.WriteLine(degreeRequiredCredits);
+            Console.WriteLine(degreeWorkload);
+
+            Console.WriteLine(programName);
+            Console.WriteLine(programDegrees);
+            Console.WriteLine(programDepartmentHead);
+
+            Console.WriteLine(courseName);
+            Console.WriteLine(courseCondition);
+
+            Console.ReadLine(); //Just so the window doesn't automatically close after execution
         }
     }
 }
